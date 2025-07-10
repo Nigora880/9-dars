0b1738da2cb7f4355b58267b5fcc2cb4074b63e3
Nigora880 <nx165880@gmail.com>
2025-07-10 16:49:40 +0300

    commit

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,12 +1,27 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './hero.css';
+import { Navigation } from 'swiper/modules';
+
+
+
+
 const Hero = () => {
     return(
-        <section className="hero-section ">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>        <section className="hero-section ">
             <div className="container flex flex-col ">
                <strong className="font-normal !pt-[164px] text-[20px] leading-[34.3px] text-[#111111] pb-[15px] ">Face Makeup Brush</strong>
                <h1 className="font-normal text-[50px] leading-[62.5px] text-[#111111] pb-[45px]">Skincare Brush Set <br />Sale 30% Off</h1>
               <button className=" bg-none text-black font-bold text-[20px] rounded-0 w-[138px] h-[63px] border-b-emerald-950 border-x-2 border-y-2">Shop now</button>
             </div>
-        </section>
+        </section></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+
     )
 }
 
--- src/modules/index.jsx
+++ src/modules/index.jsx
@@ -2,4 +2,5 @@
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero"
 import Shipping from "./Shipping"
+
 export {Header,Hero,Shipping}
